af159b710cf2011bff8d01d5d58f39e75cd10118
Noel Georgi <git@frezbo.dev>
2022-05-12 16:00:56 +0530

    chore: use aqua experimental exec mode
    Use aqua experimental `exec` mode
    
    Ref: https://github.com/aquaproj/aqua/issues/710#issuecomment-1123784696
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,6 +6,7 @@ end
 
 # aqua
 set -x AQUA_GLOBAL_CONFIG "/home/frezbo/.config/aquaproj-aqua/aqua.yaml"
+set -x AQUA_EXPERIMENTAL_X_SYS_EXEC "true"
 
 set starship_precmd_user_func "set_win_title"
 starship init fish | source
