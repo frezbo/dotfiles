45985a15a719eee4bc196bc31ea0a91c121ec927
Noel Georgi <git@frezbo.dev>
2022-05-17 18:37:13 +0530

    chore: add git autostash
    Add git autostash for [pull with rebase](https://cscheng.info/2017/01/26/git-tip-autostash-with-git-pull-rebase.html)
    General cleanup
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- dot_config/dotfiles/fzf/fzf.bash
+++ dot_config/dotfiles/fzf/fzf.bash
@@ -1,5 +1,3 @@
-# BEGIN ANSIBLE MANAGED BLOCK
 [[ \$- == *i* ]] && source "/home/frezbo/.config/dotfiles/fzf/completion.bash" 2> /dev/null
 
 source /home/frezbo/.config/dotfiles/fzf/key-bindings.bash
-# END ANSIBLE MANAGED BLOCK
